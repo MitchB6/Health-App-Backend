81fc456a0691e1026861ce139194346e64c9a018
Diep Nguyen <144938188+DiepDN94@users.noreply.github.com>
2023-11-16 11:35:48 -0500

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,3 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
 
 db = SQLAlchemy()
+Base = automap_base()
+
+def init_app(app):
+    db.init_app(app)
+
+    with app.app_context():
+        Base.prepare(db.engine, reflect=True)
