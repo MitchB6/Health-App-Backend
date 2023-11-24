9d2a2d453b9ef5cb7e39ff13ff2d610f03221837
Diep Nguyen <144938188+DiepDN94@users.noreply.github.com>
2023-11-24 16:03:33 -0500

    Seperated routes into auth.py and member.py

--- config.py
+++ config.py
@@ -7,13 +7,7 @@ class Config:
     SECRET_KEY=config('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS=config('SQLALCHEMY_TRACK_MODIFICATIONS', cast=bool)
    
-    """MySQL"""
     SQLALCHEMY_DATABASE_URI = config('MYSQL_DATABASE_URI')
-    
-    """SQLite"""
-    SQLALCHEMY_BINDS = {
-        'pictures': config('SQLITE_DATABASE_URI', default=f"sqlite:///"+os.path.join(BASE_DIR, 'dev.db'))
-    }
     
 class DevConfig(Config):
     DEBUG=True
@@ -24,6 +18,8 @@ class ProdConfig(Config):
     pass
 
 class TestConfig(Config):
-    TESTING = True
+    SQLALCHEMY_DATABASE_URI='MYSQL_DATABASE_URI'
+    SQLALCHEMY_ECHO=False
+    TESTING=True
     pass
 
--- src/routes.py
+++ src/routes.py
@@ -1,77 +1,17 @@
-from flask import jsonify,request
-from flask_restx import Resource,fields
-from .extensions import api,bcrypt
-from .serializers import member_model, password_model
-from .models import Member,Password
+from flask import jsonify
+from flask_restx import Resource
 
-signup_model=api.model(
-    "SignUp",
-    {
-        "email":fields.String,
-        "password":fields.String()
-    }
-)
+from .extensions import api
 
-workout_model=api.model(
-    "Workout",
-    {
-        "name":fields.String()
-    }
-)
-
-member_model=api.model(
-    "Member",
-    {
-        "first_name":fields.String(),
-        "last_name":fields.String()
-    }
-)
-    
 @api.route('/')
 class HomeResource(Resource):
     def get(self):
-        return {"message":"Welcome to our fitness app"}
+        """Homepage"""
+        return jsonify(message="hello world")
 
 @api.route('/health')
 class HealthResource(Resource):
   def get(self):
     """Health check of backend"""
-    return {'status': 'success', 'message': 'Health check success'}
-
-@api.route('/members/<int:id>')
-class MemberResource(Resource):
-    def get(self,id):
-        """Get a member by id"""
-        pass
-    
-    def put(self,id):
-        """Update a member by id"""
-        pass
-    
-    def delete(self,id):
-        """Delete a member by id"""
-        pass
-    
-@api.route('/signup')
-class SignUp(Resource):
-    @api.expect(signup_model)
-    def post(self):
-        data=request.get_json()
-        email=data.get('email')
+    return jsonify({'status': 'success', 'message': 'Health check success'})
         
-        db_user=Member.query.filter_by(email=email).first()
-        if db_user is not None:
-            return jsonify({"message":f"Email {email} already exists"})
-        
-        new_member=Member(
-            email=data.get('email')
-        )
-        new_member.save(flush=True)
-        new_password=Password(
-            member_id=new_member.member_id,
-            hashed_pw=bcrypt.generate_password_hash(data.get('password'))
-        )
-        new_password.save()
-        new_member.commit()
-        
-        return jsonify({"message": "User created successfully"})
