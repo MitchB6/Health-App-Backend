d1f8a7a4da0b2fd2a62b0f5f0f2bdcaccfe5c8f2
Diep Nguyen <144938188+DiepDN94@users.noreply.github.com>
2023-11-25 21:15:24 -0500

    Added endpoints for getting and updating goals and workouts

--- src/auth.py
+++ src/auth.py
@@ -1,11 +1,12 @@
 from flask import jsonify,request,make_response
 from flask_restx import Resource
-from flask_jwt_extended import (create_access_token,
+from flask_jwt_extended import (JWTManager, create_access_token,
 create_refresh_token,jwt_required,get_jwt_identity)
 from flask_bcrypt import Bcrypt
 
 from .serializers import signup_model,login_model,auth_ns
 from .models import Member,Password
+from .extensions import db
 
 bcrypt=Bcrypt()
 
@@ -13,69 +14,90 @@ bcrypt=Bcrypt()
 class SignUp(Resource):
     @auth_ns.expect(signup_model)
     def post(self):
-        """User signup with email and password"""
-        data=request.get_json()
+      """User signup with email and password"""
+      data=request.get_json()
+      email=data.get('email')
+      
+      db_user=Member.query.filter_by(email=email).first()
+      if db_user is not None:
+        return jsonify({"message":f"Email {email} already exists"})
+      
+      new_member=Member(
         email=data.get('email')
-        
-        db_user=Member.query.filter_by(email=email).first()
-        if db_user is not None:
-            return jsonify({"message":f"Email {email} already exists"})
-        
-        new_member=Member(
-            email=data.get('email')
-        )
-        new_member.save(flush=True)
-        new_password=Password(
-            member_id=new_member.member_id,
-            hashed_pw=bcrypt.generate_password_hash(data.get('password'))
-        )
-        new_password.save()
-        new_member.save(commit=True)
-        
-        return jsonify({"message": "User created successfully"})
+      )
+      new_member.save(flush=True)
+      new_password=Password(
+        member_id=new_member.member_id,
+        hashed_pw=bcrypt.generate_password_hash(data.get('password'))
+      )
+      new_password.save()
+      new_member.save(commit=True)
+      
+      return jsonify({"message": "User created successfully"})
     
 @auth_ns.route('/login')
 class Login(Resource):
-    @auth_ns.expect(login_model)
-    def post(self):
-        """User login using email and password"""
-        data=request.get_json()
+  @auth_ns.expect(login_model)
+  def post(self):
+    """User login using email and password"""
+    data=request.get_json()
+    
+    email=data.get('email')
+
+    password=data.get('password')
+
+    db_user=Member.query.filter_by(email=email).first()
+
+    if db_user:
+      db_password = db_user.passwords
+
+      if db_user and bcrypt.check_password_hash(db_password.hashed_pw, password):
+        """JWT_ACCESS_TOKEN_EXPIRES default(15 minutes)"""         
+        access_token=create_access_token(identity=db_user.member_id)
+        refresh_token=create_refresh_token(identity=db_user.member_id)
         
-        email=data.get('email')
-
-        password=data.get('password')
-
-        db_user=Member.query.filter_by(email=email).first()
-
-        if db_user:
-            db_password = db_user.passwords
-
-            if db_user and bcrypt.check_password_hash(db_password.hashed_pw, password):
-                """JWT_ACCESS_TOKEN_EXPIRES default(15 minutes)"""         
-                access_token=create_access_token(identity=db_user.member_id)
-                refresh_token=create_refresh_token(identity=db_user.member_id)
-                
-                print("Access Token:", access_token)
-                print("Refresh Token:", refresh_token)
-                
-                response_data={
-                    "access token": access_token,
-                    "refresh token": refresh_token
-                }
-                print("Response Data:", response_data)
-                return jsonify(response_data)
-            else:
-                return jsonify({"message":"Invalid credentials"}), 401
-        else:
-            return jsonify({"message": "User not found"}), 404
+        print("Access Token:", access_token)
+        print("Refresh Token:", refresh_token)
+        
+        response_data={
+          "access token": access_token,
+          "refresh token": refresh_token
+        }
+        print("Response Data:", response_data)
+        return jsonify(response_data)
+      else:
+        return jsonify({"message":"Invalid credentials"}), 401
+    else:
+      return jsonify({"message": "User not found"}), 404
 
 @auth_ns.route('/refresh')
 class RefreshResource(Resource):
-    @jwt_required(refresh=True)
-    def post(self):
-        
-        current_user=get_jwt_identity()
-        
-        new_access_token=create_access_token(identity=current_user)
-        
-        return make_response(jsonify({"access_token":new_access_token}),200)
+  @jwt_required(refresh=True)
+  def post(self):
+    """User gets new access token with refresh token"""
+    current_user=get_jwt_identity()
+    
+    new_access_token=create_access_token(identity=current_user)
+    
+    return make_response(jsonify({"access_token":new_access_token}),200)
+
+@auth_ns.route('/change_password')
+class ChangePasswordResource(Resource):
+  def post(self):
+    """Endpoint for a a member to change their password"""
+    current_member_id = get_jwt_identity()
+    data=request.get_json()
+    
+    member=Member.query.get_or_404(current_member_id)
+    current_password=member.passwords.hashed_pw
+    
+    old_password=data.get('old_password')
+    if not bcrypt.check_password_hash(current_password, old_password):
+      return jsonify({"message":"Old password is incorrect"}), 401
+    
+    new_password=data.get('new_password')
+    hashed_new_password=bcrypt.generate_password_hash(new_password)
+    current_password.hashed_pw=hashed_new_password
+    db.session.commit()
+    
+    return jsonify({"message":"Password changed successfully"}), 200
--- src/member.py
+++ src/member.py
@@ -1,45 +1,107 @@
 from flask_restx import Resource
-from flask import request
-from flask_jwt_extended import jwt_required
+from flask import request,jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from datetime import datetime, timedelta
 
 from .models import Member
 from .serializers import member_model, member_ns
 
+
 @member_ns.route('/<int:id>')
-class MemberResource(Resource):
-    @member_ns.marshal_with(member_model)
-    def get(self,id):
-        """
-        The function `get` retrieves a member from the database based on their id and returns it.
-        
-        :param id: The id parameter is the unique identifier of the member that we want to retrieve from the
-        database
-        :return: The member object with the specified id is being returned.
-        """
-        member=Member.query.get_or_404(id)
-        return member
-    
-    @member_ns.marshal_with(member_model)
-    @jwt_required()
-    def put(self,id):
-        """
-        The `put` function updates a member in the database based on their ID.
-        
-        :param id: The `id` parameter is the unique identifier of the member that needs to be updated
-        """
-        member_to_update=Member.query.get_or_404(id)
-        data=request.get_json()
-        member_to_update.update(**data)
+class MemberSettingsResource(Resource):
+  @member_ns.marshal_with(member_model)
+  def get(self):
+    """
+    Retrieves the settings for the current logged-in member.
+    :return: The settings of the current member.
+    """
+    current_member_id =  get_jwt_identity()
+    member=Member.query.get_or_404(current_member_id)
+    return member
+  
+  @member_ns.marshal_with(member_model)
+  @jwt_required()
+  def put(self):
+    """
+    Updates the settings for the current logged-in member.
+    :return: The updated member data.
+    """
+    current_member_id =  get_jwt_identity()
+    member=Member.query.get_or_404(current_member_id)
+    data=request.get_json()
+    member.update(**data)
+    return member, 200
             
-    @member_ns.marshal_with(member_model)
-    def delete(self,id):
-        """
-        The above function deletes a recipe with a given ID from the database and returns the deleted
-        recipe.
-        
-        :param id: The `id` parameter is the unique identifier of the recipe that needs to be deleted
-        :return: The recipe that was deleted is being returned.
-        """
-        recipe_to_delete=Member.query.get_or_404(id)
-        recipe_to_delete.delete()
-        return recipe_to_delete
+  def delete(self,id):
+    current_member_id =  get_jwt_identity()
+    member=Member.query.filter_by(id=current_member_id, is_active=True).first_or_404()
+    member.delete()
+    return jsonify({"message": "Member deletion requested successfully. Your account will be permanently deleted after 30 days unless you cancel the request."}), 200
+  
+  def cancel_delete(self,id):
+    current_member_id =  get_jwt_identity()
+    member=Member.query.filter_by(id=current_member_id, is_active=False).first_or_404()
+    if member.delete_requested_at and datetime.utcnow() < member.delete_requested_at + timedelta(days=30):
+      member.cancel_delete()
+      return jsonify({"message": "Member deletion request cancelled successfully."}), 200
+    return jsonify({"message": "Member deletion request not found or has expired."}), 404 
+  
+  def perform_delete():
+    threshold_date = datetime.utcnow
+    members_to_delete = Member.query.filter(Member.is_active==False, Member.delete_requested_at <= threshold_date).all()
+    
+    for member in members_to_delete:
+      member.hard_delete()
+      
+    return jsonify({'message': f'{len(members_to_delete)} members deleted successfully'}), 200
+
+@member_ns.route('/<int:id>/goals')
+class MemberGoalsResource(Resource):
+  @member_ns.marshal_with(member_model)
+  def get(self):
+    """
+    Retrieves the goals for the current logged-in member.
+    :return: The goals of the current member.
+    """
+    current_member_id =  get_jwt_identity()
+    member=Member.query.get_or_404(current_member_id)
+    return member
+  
+  @member_ns.marshal_with(member_model)
+  @jwt_required()
+  def put(self):
+    """
+    Updates the goals for the current logged-in member.
+    :return: The updated member data.
+    """
+    current_member_id =  get_jwt_identity()
+    member=Member.query.get_or_404(current_member_id)
+    data=request.get_json()
+    member.update(**data)
+    return member, 200
+
+@member_ns.route('/<int:id>/workouts')
+class WorkoutsResource(Resource):
+  @member_ns.marshal_with(member_model)
+  def get(self):
+    """
+    Retrieves the workouts for the current logged-in member.
+    :return: The workouts of the current member.
+    """
+    current_member_id =  get_jwt_identity()
+    member=Member.query.get_or_404(current_member_id)
+    return member
+  
+  @member_ns.marshal_with(member_model)
+  @jwt_required()
+  def put(self):
+    """
+    Updates the workouts for the current logged-in member.
+    :return: The updated member data.
+    """
+    current_member_id =  get_jwt_identity()
+    member=Member.query.get_or_404(current_member_id)
+    data=request.get_json()
+    member.update(**data)
+    return member, 200
+
