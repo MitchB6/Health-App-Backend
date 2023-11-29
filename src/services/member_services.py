55ae1cf9955f0246702bee687e10dc3f3a580e89
Diep Nguyen <144938188+DiepDN94@users.noreply.github.com>
2023-11-29 17:25:16 -0500

    update to member_model

--- src/services/member_services.py
+++ src/services/member_services.py
@@ -1,24 +1,24 @@
+from flask import jsonify, make_response
 from flask_jwt_extended import get_jwt_identity
-import re #regex
 
-from ..models.member_model import Member
+from ..models.personalinfo_model import PersonalInfo
 from .validations import *
 
 def get_member_settings():
-    current_member_id = get_jwt_identity()
-    if current_member_id is None:
-        return {"message": "Member not found."}
-    member = Member.query.get_or_404(current_member_id)
-    return member, 200
+  current_member_id = get_jwt_identity()
+  if current_member_id is None:
+    return {"message": "Member not found."}
+  result, status_code = PersonalInfo.query.get_or_404(current_member_id)
+  return make_response(jsonify(result, status_code))
 
 def update_member_settings(data):
-    current_member_id = get_jwt_identity()
-    member = Member.query.get_or_404(current_member_id)
-    member.update(**data)
-    return {"message": "Member settings updated successfully."}, 200
+  current_member_id = get_jwt_identity()
+  member = PersonalInfo.query.get_or_404(current_member_id)
+  result, status_code = member.update(**data)
+  return {"message": "Member settings updated successfully."}, 200
 
 def delete_member():
-    current_member_id = get_jwt_identity()
-    member = Member.query.filter_by(member_id=current_member_id).first_or_404()
-    member.delete()
-    return {"message": "Member deleted successfully."}, 200
+  current_member_id = get_jwt_identity()
+  member = PersonalInfo.query.filter_by(member_id=current_member_id).first_or_404()
+  member.delete()
+  return {"message": "Member deleted successfully."}, 200
