5e5715d45ee05d690e5d92b1ad631c54b98e8d21
KarimSalem9000 <KarimSalem9000@gmail.com>
2023-12-20 22:44:33 -0500

    done

--- src/routes/chat.py
+++ src/routes/chat.py
@@ -17,6 +17,51 @@ class Chat(Resource):
   def get(self):
     return jsonify({'message': 'chat check success'})
 
+def get_chats_by_key(chat_key):
+    try:
+        # Query the database for messages with the specified chat_key
+        chats_by_key = Chats.query.filter_by(chatkey=chat_key).all()
+
+        # Convert the records to a list of dictionaries
+        chats_list = [
+            {
+                'chatkey': chat.chatkey,
+                'sender': chat.sender,
+                'recipient': chat.recipient,
+                'message': chat.message,
+                'timestamp': chat.timestamp.isoformat()  # Assuming there is a timestamp field
+            }
+            for chat in chats_by_key
+        ]
+
+        return chats_list
+    except Exception as e:
+        print(f"Error retrieving chats for key {chat_key}: {e}")
+        return None
+
+@chat_ns.route('/history/<chat_key>')
+class ChatHistory(Resource):
+    @jwt_required()
+    def get(self, chat_key):
+        try:
+            # Query the database for messages with the specified chat_key
+            messages = Chats.query.filter_by(chatkey=chat_key).all()
+
+            # Convert the messages to a list of dictionaries
+            message_list = [
+                {
+                    'chatkey': message.chatkey,
+                    'sender': message.sender,
+                    'recipient': message.recipient,
+                    'message': message.message,
+                }
+                for message in messages
+            ]
+
+            return jsonify({'chat_history': message_list})
+        except Exception as e:
+            print(f"Error retrieving messages: {e}")
+            return make_response(jsonify({'error': 'Unable to fetch chat history'}), 500)
 
 @socketio.on('send_message')
 def handle_send_message(data):
@@ -27,6 +72,18 @@ def handle_send_message(data):
 
   chat_key = f'{sender}-{recipient}'
 
+
+
+  print("this is data",data)
+  print("this the key",chat_key)
+  print("this the history",chat_history)
+
+  if chat_key not in chat_history:
+    chat_history[chat_key] = []
+  chat_history[chat_key].append(data)
+
+  emit('new_message', data, room=recipient)
+  emit('new_message', data, room=sender)
   new_message = Chats(chatkey=chat_key, sender=sender,
                       recipient=recipient, message=text)
 
@@ -34,28 +91,21 @@ def handle_send_message(data):
   db.session.add(new_message)
   db.session.commit()
 
-  print(data)
-  print(chat_key)
-  print(chat_history)
-
-  if chat_key not in chat_history:
-    chat_history[chat_key] = []
-    chat_history[chat_key].append(data)
-
-  emit('new_message', data, room=recipient)
-  emit('new_message', data, room=sender)
-
 
 @socketio.on('request_history')
 def handle_request_history(data):
+  print("\n\n\n testing if this works \n request_history \n\n\n")
   user1 = data['user1']
   user2 = data['user2']
-  chat_key = tuple(sorted([user1, user2]))
-  print(chat_key)
+  chat_key = f'{user1}-{user2}'
+  #chat_key = tuple(sorted([user1, user2]))
+  print("chat key",chat_key)
   print("-".join(chat_key))
+  chat_historys = get_chats_by_key("user1-user2")
+  
 
   history = chat_history.get(chat_key, [])
-  emit('chat_history', history, room=chat_key)
+  emit('chat_history', chat_historys, room=chat_key)
 
 
 @socketio.on('connect')
