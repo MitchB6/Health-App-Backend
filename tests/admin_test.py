fb14a3ca45a621f1a341f8d26268bc267c8c65a7
MusaSShaikh <90275557+MusaSShaikh@users.noreply.github.com>
2023-12-21 02:42:57 -0200

    Update admin_test.py

--- tests/admin_test.py
+++ tests/admin_test.py
@@ -10,56 +10,58 @@ from config import TestConfig
 
 @pytest.fixture(scope='module')
 def app():
-  _app = create_app(TestConfig)
-  ctx = _app.app_context()
-  ctx.push()
-  yield _app
-  ctx.pop()
+    _app = create_app(TestConfig)
+    ctx = _app.app_context()
+    ctx.push()
+    yield _app
+    ctx.pop()
 
 
 @pytest.fixture
 def client(app):
-  return app.test_client()
+    return app.test_client()
 
 
 @pytest.fixture
 def admin_token(app):
-  # Mock JWT token creation
-  with app.app_context():
-    additional_claims = {"role_id": 2}
-    return create_access_token(identity="admin_user", additional_claims=additional_claims)
+    # Mock JWT token creation
+    with app.app_context():
+        additional_claims = {"role_id": 2}
+        return create_access_token(identity="admin_user", additional_claims=additional_claims)
 
 
+@pytest.mark.usefixtures("mocker")
 def test_get_all_coach_forms(client, admin_token, mocker):
-  # Mock the get_all_coach_forms service
-  mocker.patch(
-      'src.routes.admin.get_all_coach_forms',
-      return_value=({"message": "Success"}, 200)
-  )
+    # Mock the get_all_coach_forms service
+    mocker.patch(
+        'src.routes.admin.get_all_coach_forms',
+        return_value=({"message": "Success"}, 200)
+    )
 
-  response = client.get(
-      '/admin/',
-      headers={"Authorization": f"Bearer {admin_token}"}
-  )
+    response = client.get(
+        '/admin/',
+        headers={"Authorization": f"Bearer {admin_token}"}
+    )
 
-  assert response.status_code == 200
-  assert b"Success" in response.data
+    assert response.status_code == 200
+    assert b"Success" in response.data
 
 
+@pytest.mark.usefixtures("mocker")
 def test_approve_coach_form(client, admin_token, mocker):
-  # Mock the update_coach service
-  mocker.patch(
-      'src.routes.admin.update_coach',
-      return_value=({"message": "Coach Approved"}, 200)
-  )
+    # Mock the update_coach service
+    mocker.patch(
+        'src.routes.admin.update_coach',
+        return_value=({"message": "Coach Approved"}, 200)
+    )
 
-  data = {"coach_id": 1, "approved": True}
-  response = client.put(
-      '/admin/',
-      headers={"Authorization": f"Bearer {admin_token}"},
-      data=json.dumps(data),
-      content_type='application/json'
-  )
+    data = {"coach_id": 1, "approved": True}
+    response = client.put(
+        '/admin/',
+        headers={"Authorization": f"Bearer {admin_token}"},
+        data=json.dumps(data),
+        content_type='application/json'
+    )
 
-  assert response.status_code == 200
-  assert b"Coach Approved" in response.data
+    assert response.status_code == 200
+    assert b"Coach Approved" in response.data
